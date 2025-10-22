ee513724731cef3531a988a72d028be9297f6009
Tijesunimi Peters <tijesunimipeters@gmial.com>
2025-10-22 01:45:55 -0700

    Switch to using SSR cos of env vars

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
 set -e
 
+export NODE_PORT=$PORT
+
 # Substitute the PORT env var into nginx.conf.template
-envsubst '$PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+#envsubst '$PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Start Nginx in the foreground
-exec nginx -g 'daemon off;'
+#exec nginx -g 'daemon off;'
+
+yarn start
