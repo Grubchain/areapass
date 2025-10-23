bc59e42c8cc3e6f4da8c2727e903d80af5e708e0
Tijesunimi Peters <tijesunimipeters@gmial.com>
2025-10-23 00:31:50 -0700

    Save build command for heroku

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -8,10 +8,6 @@ export SERVER_PORT="${PORT}"
 
 echo "[entrypoint] SERVER_PORT=$SERVER_PORT"
 
-if [ "$#" -gt 0 ]; then
-  exec "$@"
-fi
-
 envsubst '$SERVER_PORT' < "/etc/nginx/templates/port.conf.template" > "/etc/nginx/port.conf"
 
 cp /etc/nginx/templates/nginx.conf.template /etc/nginx/nginx.conf
