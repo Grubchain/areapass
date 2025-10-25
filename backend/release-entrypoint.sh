b5c39405d2f03d9b6053047c7fb75f1eea797f43
Tijesunimi Peters <tijesunimipeters@gmial.com>
2025-10-24 21:39:56 -0700

    Setup up ssh tunnel for release

--- backend/release-entrypoint.sh
+++ backend/release-entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+/usr/bin/autossh -M 0 -N -L 15432:$SSH_REMOTE_HOST:5432 $SSH_TUNNEL_USER@$SSH_TUNNEL_HOST -i $SSH_TUNNEL_PEM
+
 if ! php artisan migrate --force; then
     echo "============================================"
     echo "ERROR: Migrations could not complete. Check the error above."
