b75d268b9f752d4d00318d49949e7f650f65b48f
Tijesunimi Peters <tijesunimipeters@gmial.com>
2025-10-26 00:47:54 -0700

    Everything working via containers but database have to created manually

--- backend/release-entrypoint.sh
+++ backend/release-entrypoint.sh
@@ -2,17 +2,4 @@
 
 set -eu
 
-/usr/bin/autossh -M 0 -N -L 15432:$SSH_REMOTE_HOST:5432 $SSH_TUNNEL_USER@$SSH_TUNNEL_HOST -i $SSH_TUNNEL_PEM
-
-if ! php artisan migrate --force; then
-    echo "============================================"
-    echo "ERROR: Migrations could not complete. Check the error above."
-    echo "Ensure DATABASE_URL is set."
-    echo "============================================"
-fi
-
-php artisan cache:clear
-php artisan config:clear
-php artisan route:clear
-php artisan view:clear
-php artisan storage:link
+exec /usr/bin/supervisord -c /etc/release-supervisord.conf
